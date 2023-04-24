700cd5c84d12d29dd00ec7c501ef58c295fde0e2
dylantylr <taylod9@rpi.edu>
2023-04-23 21:58:18 -0400

    yas

---
+++ src/assets/index.js
@@ -0,0 +1,66 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import david_goggins from "./david_goggins.jpg";
+import shuzo from "./shuzo.jpg";
+import denzel from "./denzel.jpg";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import tableau from "./tech/tableau.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import java from "./tech/java.png";
+import matlab from "./tech/matlab.png";
+import r from "./tech/r.png";
+import c from "./tech/c.png";
+import sql from "./tech/sql.png";
+import cpp from "./tech/cpp.png";
+import python from "./tech/python.png";
+
+import carvel from "./company/carvel.png";
+import sherwin from "./company/sherwin.png";
+import rpi from "./company/rpi.png";
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  tableau,
+  git,
+  html,
+  javascript,
+  java,
+  matlab,
+  r,
+  c,
+  sql,
+  cpp,
+  python,
+  carvel,
+  sherwin,
+  rpi,
+  carrent,
+  jobit,
+  tripguide,
+  david_goggins,
+  shuzo,
+  denzel,
+};
---
+++ src/constants/index.js
@@ -0,0 +1,249 @@
+import {
+    mobile,
+    backend,
+    creator,
+    web,
+    css,
+    docker,
+    tableau,
+    git,
+    html,
+    javascript,
+    java,
+    matlab,
+    r,
+    c,
+    sql,
+    cpp,
+    python,
+    sherwin,
+    rpi,
+    carvel,
+    carrent,
+    jobit,
+    tripguide,
+    david_goggins,
+    shuzo,
+    denzel,
+  } from "../assets";
+  
+  export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "work",
+      title: "Work",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+  
+  const services = [
+    {
+      title: "Student",
+      icon: web,
+    },
+    {
+      title: "Software Engineer",
+      icon: mobile,
+    },
+    {
+      title: "Backend Developer",
+      icon: backend,
+    },
+    {
+      title: "Data Analyst",
+      icon: creator,
+    },
+  ];
+  
+  const technologies = [
+    {
+      name: "Python",
+      icon: python,
+    },
+    {
+      name: "C++",
+      icon: cpp,
+    },
+    {
+      name: "SQL",
+      icon: sql,
+    },
+    {
+      name: "C",
+      icon: c,
+    },
+    {
+      name: "R",
+      icon: r,
+    },
+    {
+      name: "Java",
+      icon: java,
+    },
+    {
+      name: "Matlab",
+      icon: matlab,
+    },
+    {
+      name: "Tableau",
+      icon: tableau,
+    },
+    {
+      name: "git",
+      icon: git,
+    },
+    {
+      name: "HTML 5",
+      icon: html,
+    },
+    {
+      name: "CSS 3",
+      icon: css,
+    },
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+    {
+      name: "docker",
+      icon: docker,
+    },
+  ];
+  
+  const experiences = [
+    {
+      title: "Digital Technology Intern/Co-op",
+      company_name: "Sherwin Williams",
+      icon: sherwin,
+      iconBg: "#383E56",
+      date: "March 2023 - August 2023",
+      points: [
+        "Worked with Senior & Jr. Developers to create a data visualization dashboard using Tableau, SQL, and Python to enhance business metrics analysis and decision-making processes.",
+      ],
+    },
+    {
+      title: "CAD Teaching Assistant",
+      company_name: "Rensselaer Polytechnic Institute",
+      icon: rpi,
+      iconBg: "#E6DEDD",
+      date: "January 2022 - December 2022",
+      points: [
+        "Educated 50 undergraduate students weekly on the fundamentals of CAD software.",
+        "Provided comprehensive feedback on students’ assignments.",
+        "Conducted weekly office hours to work through questions students had.",
+      ],
+    },
+    {
+      title: "Server and Cashier",
+      company_name: "Carvel Ice Cream",
+      icon: carvel,
+      iconBg: "#383E56",
+      date: "October 2019 - August 2021",
+      points: [
+        "Ensured customer satisfaction while processing orders during busy hours.",
+        "Managed inventory of products and planned weekly orders.",
+        "Supervised other staff during shifts worked.",
+      ],
+    },
+  ];
+  
+  const testimonials = [
+    {
+      testimonial:
+        "Nobody cares what you did yesterday. What have you done today to better yourself?",
+      name: "David Goggins",
+      designation: "",
+      company: "",
+      image: david_goggins,
+    },
+    {
+      testimonial:
+        "In that case, hold on for dear life! Stake your entire life in here and now!",
+      name: "Shuzo Matsuoka",
+      designation: "",
+      company: "",
+      image: shuzo,
+    },
+    {
+      testimonial:
+        "Dreams without goals, are just dreams and they ultimately fuel disappointment.",
+      name: "Denzel Washington",
+      designation: "",
+      company: "",
+      image: denzel,
+    },
+  ];
+  
+  const projects = [
+    {
+      name: "Meta Back-End Developer Professional Certificate",
+      description:
+        "The Meta Back-End Developer Certificate is awarded to learners who complete a series of courses on back-end development, hosted by Coursera. The courses culminate in the successful completion of a capstone project.",
+      tags: [
+        {
+          name: "Django",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "APIs",
+          color: "green-text-gradient",
+        },
+        {
+          name: "MySQL",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: carrent,
+      source_code_link: "https://coursera.org/share/f99548cdb5936763a9132e2390e459b7",
+    },
+    {
+      name: "Microsoft Certified: Azure AI Fundamentals",
+      description:
+        "Earners of the Azure AI Fundamentals certification have demonstrated foundational knowledge of machine learning (ML) and artificial intelligence (AI) concepts and related Microsoft Azure services.",
+      tags: [
+        {
+          name: "Machine Learning",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "AI",
+          color: "green-text-gradient",
+        },
+        {
+          name: "Computer Vision",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: tripguide,
+      source_code_link: "https://github.com/",
+    },
+    {
+      name: "Microsoft Certified: Azure Fundamentals",
+      description:
+        "Earners of the Azure Fundamentals certification have demonstrated foundational level knowledge of cloud services and how those services are provided with Microsoft Azure.",
+      tags: [
+        {
+          name: "Azure",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "Cloud Services",
+          color: "green-text-gradient",
+        },
+        {
+          name: "Cloud Concepts",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: jobit,
+      source_code_link: "https://github.com/",
+    },
+  ];
+  
+  export { services, technologies, experiences, testimonials, projects };
