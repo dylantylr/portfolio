ba9e9c57097f7e36011a3758bba6a571d2fdc0de
dylantylr <taylod9@rpi.edu>
2023-05-23 16:58:13 -0400

    hope

---
+++ src/assets/index.js
@@ -0,0 +1,68 @@
+import logo from "./logo.svg";
+import backend from "./backend.svg";
+import creator from "./creator.svg";
+import mobile from "./mobile.svg";
+import web from "./web.svg";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import david_goggins from "./david_goggins.jpg";
+import shuzo from "./shuzo.jpg";
+import denzel from "./denzel.jpg";
+import website from "./website.png";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import tableau from "./tech/tableau.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import java from "./tech/java.png";
+import matlab from "./tech/matlab.png";
+import r from "./tech/r.png";
+import c from "./tech/c.png";
+import sql from "./tech/sql.png";
+import cpp from "./tech/cpp.png";
+import python from "./tech/python.png";
+
+import carvel from "./company/carvel.png";
+import sherwin from "./company/sherwin.png";
+import rpi from "./company/rpi.png";
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  tableau,
+  git,
+  html,
+  javascript,
+  java,
+  matlab,
+  r,
+  c,
+  sql,
+  cpp,
+  python,
+  carvel,
+  sherwin,
+  rpi,
+  carrent,
+  jobit,
+  tripguide,
+  david_goggins,
+  shuzo,
+  denzel,
+  website,
+};
