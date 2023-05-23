f48d3cb38ea475c6af127010f651fffe7388b8da
dylantylr <taylod9@rpi.edu>
2023-05-23 16:44:42 -0400

    slay 2

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -25,7 +25,7 @@ const ServiceCard = ({ index, title, icon }) => (
         <img
           src={icon}
           alt='web-development'
-          className='w-16 h-16 object-contain'
+          className='w-17 h-16 object-contain'
         />
 
         <h3 className='text-white text-[20px] font-bold text-center'>
