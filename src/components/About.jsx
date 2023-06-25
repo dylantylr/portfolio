cc324389187d4d8869194426fef721a1f99263cb
dylantylr <taylod9@rpi.edu>
2023-06-24 20:24:23 -0400

    update 6-24

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -48,11 +48,7 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
       >
-        I am currently a Senior in the School of Science at Rensselaer Polytechnic Institute, 
-        working towards a B.S. in both Computer Science and Economics.
-        I'm driven by a passion for using computational tools and mathematical principles 
-        to tackle the challenges of everyday life. I'm excited to explore the intersection 
-        of these fields and discover new ways to make a positive impact on the world.
+        As a Senior at Rensselaer Polytechnic Institute's School of Science, I am diligently working towards a B.S. in both Computer Science and Economics. My journey is fueled by a passion for harnessing the power of computational tools and mathematical principles to address real-world challenges.  As I aspire to become a software engineer, I am excited about the opportunities that lie ahead to leverage my skills and education to drive technological innovation. 
       </motion.p>
 
       <div className='mt-20 flex flex-wrap gap-10 place-content-center'>
