71edeebbcea4c0f0f60cbacb65be950a24cca832
dylantylr <taylod9@rpi.edu>
2024-07-05 00:56:34 -0400

    mashle

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,25 +1,41 @@
-import React, { Suspense } from "react";
-import { Canvas } from "@react-three/fiber";
+import React, { useRef, useEffect, Suspense } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
-
+import * as THREE from 'three';
 import CanvasLoader from "../Loader";
 
 const Earth = () => {
-  const earth = useGLTF("./silent_ash/scene.gltf");
+  const earthRef = useRef();
+  const { scene, animations } = useGLTF("./silent_ash/scene.gltf");
+  const mixer = useRef();
+
+  useEffect(() => {
+    if (animations.length > 0) {
+      mixer.current = new THREE.AnimationMixer(scene);
+      animations.forEach((clip) => {
+        const action = mixer.current.clipAction(clip);
+        action.play();
+      });
+    }
+  }, [animations, scene]);
+
+  useFrame((state, delta) => {
+    mixer.current?.update(delta);
+  });
 
   return (
-    <mesh>
-    <hemisphereLight intensity={0.15} groundColor='black' />
-    <spotLight
-        position={[-20, 50, 10]}
-        angle={0.12}
-        penumbra={1}
-        intensity={1}
-        castShadow
-        shadow-mapSize={1024}
-      />
-      <pointLight intensity={1} />
-    <primitive object={earth.scene} scale={2.5} position-y={-2} rotation-y={0} />
+    <mesh ref={earthRef}>
+      <hemisphereLight intensity={0.15} groundColor='black' />
+      <spotLight
+          position={[-20, 50, 10]}
+          angle={0.12}
+          penumbra={1}
+          intensity={1}
+          castShadow
+          shadow-mapSize={1024}
+        />
+        <pointLight intensity={1} />
+      <primitive object={scene} scale={2.5} position-y={-2} />
     </mesh>
   );
 };
@@ -28,19 +44,19 @@ const EarthCanvas = () => {
   return (
     <Canvas
       shadows
-      frameloop='demand'
+      frameloop='always'
       dpr={[1, 2]}
       gl={{ preserveDrawingBuffer: true }}
       camera={{
         fov: 45,
         near: 0.1,
         far: 200,
-        position: [-4, 3, 6],
+        position: [0, 3, 6],
       }}
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
-          autoRotate
+          autoRotate={false}
           enableZoom={false}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
